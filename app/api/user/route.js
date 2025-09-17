4d3b64a88260377ed2698f721d922a69228a5056
Sumit Raj <sumit.does.code@gmail.com>
2025-09-17 15:13:54 +0530

    let's see

--- app/api/user/route.js
+++ app/api/user/route.js
@@ -8,15 +8,20 @@ import { clerkClient } from "@clerk/nextjs/server";
 import mongoose from "mongoose";
 
 // get user profile
+// GET => /api/user
 export async function GET(req) {
     try {
         await connectDB();
+
         const { userId } = await auth();
         if (!userId) {
             return NextResponse.json({ success: false, message: "Unauthorized" }, { status: 401 });
         }
 
         const loggedInUser = await User.findOne({ clerkId: userId });
+        if (!loggedInUser) {
+            return NextResponse.json({ success: false, message: "User not found" }, { status: 404 });
+        }
 
         const user = await User.aggregate([
             {
@@ -49,9 +54,7 @@ export async function GET(req) {
                 },
             },
         ]);
-        if (!user) {
-            return NextResponse.json({ success: false, message: "User not found" }, { status: 404 });
-        }
+
         return NextResponse.json({ success: true, user: user[0] }, { status: 200 });
     } catch (error) {
         return NextResponse.json({ success: false, message: error.message || "Internal server error" }, { status: 500 });
@@ -59,6 +62,7 @@ export async function GET(req) {
 }
 
 // update user profile
+// PATCH => /api/user
 export async function PATCH(req) {
     try {
         await connectDB();
